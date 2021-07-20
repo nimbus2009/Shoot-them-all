bd9fbcfcc30ada71b885fa0c673bf737c3831ebc
Anuj Doddakaragi <anusharu2020@gmail.com>
2021-07-20 11:20:39 +0530

    Added background, completed P5.js lib setup

--- sketch.js
+++ sketch.js
@@ -14,13 +14,24 @@ function preload() {
 function setup() {
     createCanvas(windowWidth-20,windowHeight-20);
 
+    //--Matter.js setup
+
     engine = Engine.create();
 	world = engine.world;
 
     Engine.run(engine);
+
+    //--
 }
 function draw() {
     background(220);
 
     Engine.update(engine);
+
+    drawSprites();
+
+    imageMode(CENTER)
+
+    //--Background
+    image(bg_,width/2,height/2,width,height)
 }
