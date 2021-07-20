4b94f51b19c11f54ead95c4ee06a0e4c999b424f
Anuj Doddakaragi <anusharu2020@gmail.com>
2021-07-20 11:27:29 +0530

    Added ground

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
+//--Matter.js setup prelimnary
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
+//--
+
 var world,engine;
 
 var bg,bg_;
+
+var ground;
 
 function preload() {
     bg_=loadImage("./assets/world/bg.png");
 }
 function setup() {
+    //--Canvas
     createCanvas(windowWidth-20,windowHeight-20);
 
     //--Matter.js setup
@@ -21,6 +28,13 @@ function setup() {
 
     Engine.run(engine);
 
+    //--
+
+    //--Ground
+    ground=Matter.Bodies.rectangle(width/2,height-50,width,100,{
+        isStatic:true,
+    });
+    World.add(world,ground);
     //--
 }
 function draw() {
@@ -33,5 +47,11 @@ function draw() {
     imageMode(CENTER)
 
     //--Background
-    image(bg_,width/2,height/2,width,height)
+    image(bg_,width/2,height/2,width,height);
+
+    //--Ground display
+    fill("#F5D11F")
+    rectMode(CENTER)
+    rect(ground.position.x,ground.position.y,width,100);
+    //--
 }
