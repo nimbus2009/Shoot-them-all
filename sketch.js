022f0bf919714227b4fff431af72c01989ba5108
Anuj Doddakaragi <anusharu2020@gmail.com>
2021-07-20 11:10:41 +0530

    Added setup for matter.js lib in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var world,engine;
+
 function preload() {
 
 }
 function setup() {
     createCanvas(windowWidth-20,windowHeight-20);
+
+    engine = Engine.create();
+	world = engine.world;
+
+    Engine.run(engine);
 }
 function draw() {
-    background(51)
+    background(220);
+
+    Engine.update(engine);
 }
