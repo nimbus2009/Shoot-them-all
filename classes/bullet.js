d77fd5228b6c8ce8c491c6f8dfbceeee66001929
Anuj Doddakaragi <anusharu2020@gmail.com>
2021-08-02 18:28:27 +0530

    Added code and functions to bullet class

--- classes/bullet.js
+++ classes/bullet.js
@@ -3,6 +3,23 @@ class Bullet{
         this.x=x;
         this.y=y;
 
-        this.body=Bodies.rectangle()
+        this.body=Bodies.rectangle(this.x,this.y,20,20,{
+            isStatic:true
+        });
+        World.add(world,this.body);
+
+        this.sprite=createSprite(this.x,this.y,20,20);
+    }
+    display() {
+        pos=this.body.position;
+
+        this.sprite.position.x=pos.x;
+        this.sprite.position.y=pos.y;
+    }
+    shoot(angle) {
+        angle.mult(20);
+
+        Body.setStatic(this.body,false);
+        Body.applyForce(this.body, {x: angle.x, y: angle.y})
     }
 }
