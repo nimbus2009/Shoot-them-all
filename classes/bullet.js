392153e823cb822b8e376c699110441457a323a3
Anuj Doddakaragi <anusharu2020@gmail.com>
2021-08-13 13:24:37 +0530

    Modified shoot system

--- classes/bullet.js
+++ classes/bullet.js
@@ -20,7 +20,7 @@ class Bullet{
     }
     shoot(angle) {
         var velocity = p5.Vector.fromAngle(angle);
-        velocity.mult(17);
+        velocity.mult(25);
         Matter.Body.setStatic(this.body, false);
         //Matter.Body.applyForce(this.body, {x:0,y:0} ,{ x: velocity.x, y: velocity.y });
         Matter.Body.setVelocity(this.body,{x:velocity.x,y:velocity.y});
